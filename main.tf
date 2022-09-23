369bf230b1a8675382f636b19e92e125c18650fd
emirmandal <emir.enetel@gmail.com>
2022-09-23 23:54:42 +0200

    another try

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_security_group" "hashicat" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["1.0.0.0/1","128.0.0.0/1"]
+    cidr_blocks = ["0.0.0.0/1","128.0.0.0/1"]
   }
 
   ingress {
